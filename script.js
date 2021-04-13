d0c2a7cfc853664e220b7218a6eea827cae87fc4
lipka-coder <aivenor88@gmail.com>
2021-04-13 15:26:21 +0900

    Moved JS from CodeSandbox to VS Code

---
+++ script.js
@@ -0,0 +1,107 @@
+let currentDate = new Date();
+let h5 = document.querySelector("#day-time");
+let h6 = document.querySelector("#month-day-number");
+
+let date = currentDate.getDate();
+let hours = currentDate.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = currentDate.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let day = days[currentDate.getDay()];
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+let month = months[currentDate.getMonth()];
+h5.innerHTML = `${day}, ${hours}:${minutes}`;
+h6.innerHTML = `${month} ${date}`;
+
+function showWeather(response) {
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  let cityName = document.querySelector("#city-name");
+  cityName.innerHTML = response.data.name;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = `Wind: ${Math.round(response.data.wind.speed)}mph`;
+  let description = document.querySelector("h3");
+  description.innerHTML = response.data.weather[0].description;
+}
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let temperatureCelsius = temperatureElement.innerHTML;
+  temperatureElement.innerHTML = Math.round((temperatureCelsius * 9) / 5 + 32);
+}
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+function searchCity(city) {
+  let apiKey = "08c89d7c2dd394c882a212087337db19";
+  //let searchInput = document.querySelector("#type-city").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#type-city").value;
+  searchCity(city);
+}
+let typeCity = document.querySelector("form");
+typeCity.addEventListener("submit", handleSubmit);
+
+function showPosition(position) {
+  let apiKey = "08c89d7c2dd394c882a212087337db19";
+  let unit = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=${apiKey}&units=${unit}`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showWeather);
+}
+function getCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+let locationButton = document.querySelector("#current-location");
+locationButton.addEventListener("click", getCurrentPosition);
+
+searchCity("Warsaw");
+//function searchCity(event) {
+// event.preventDefault();
+//let cityInput = document.querySelector("#type-city");
+//let cityElement = document.querySelector("#city-name");
+//cityElement.innerHTML = cityInput.value;
+//}
+//let currentCity = document.querySelector("form");
+//currentCity.addEventListener("submit", searchCity);
+
+////function convertToCelsius(event) {
+//event.preventDefault();
+//let temperatureElement = document.querySelector("#temperature");
+//temperatureElement.innerHTML = showWeather(temperature);
+//}
+//let celsiusLink = document.querySelector("#celsius-link");
+//celsiusLink.addEventListener("click", convertToCelsius);
